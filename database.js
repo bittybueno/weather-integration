33a4eac2e59fe1ad1e030e76551bad74d0a68b81
Marissa Bueno <marissa.bueno21@gmail.com>
2018-12-01 16:00:45 -0700

    finishing touches

--- database.js
+++ database.js
@@ -2,11 +2,11 @@ var pgp = require('pg-promise')();
 
 const dbConfig = {
    host: 'localhost',
-   port: 5431,
+   port: 5432,
    database: 'weather',
    user: 'postgres',
-   password: 'Bobatime' // TODO: Fill in your PostgreSQL password here.
-                // Use empty string if you did not set a password
+   password: '' // Fill in your PostgreSQL password here. Use empty string
+                // if you did not set a password
 };
 
 var db = pgp(dbConfig);
--- server.js
+++ server.js
@@ -1,45 +1,95 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const app = express()
+const app = express();
+var db = require('../database');
 
 
 require('dotenv').load();
-var expressValidator = require('express-validator');
-app.use(expressValidator());
-
-var methodOverride = require('method-override');
-app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        var method = req.body._method;
-        delete req.body._method;
-        return method
-    }
-}));
-
-var flash = require('express-flash');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-app.use(cookieParser('csci3308'));
-app.use(session({
-    secret: 'csci3308',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {maxAge: 60000}
-}));
-app.use(flash());
 
 let apiKey = process.env.MY_API_KEY;
 
 app.use(express.static('public'));
-var index = require('./routes/index');
-app.use('/', index);
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null, temp: null, playlist: null});
+})
 
+
+app.post('/', function (req, res, ) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  
+
+  request(url, function (err, response, body ) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again', temp: null, playlist: null});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again', temp: null, playlist: null});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        let temp = `${weather.main.temp}`;
+        console.log(temp);
+        let indicator = conversion(temp);
+        //playlistURL = "5dN1bcsHyvBhViD2ANdYn9";
+        if(indicator === 0){
+          var query = 'select playlist from cold order by random() limit 1';
+          db.one(query)
+              .then(function (row) {
+                console.log(row.playlist);
+                  res.render('index', {
+                      weather: weatherText,
+                      error: null,
+                      temp: temp,
+                      playlistURL: row.playlist
+                  })
+              })
+              .catch(function (err) {
+                  res.render('index', {
+                    weather: null, 
+                    error: 'Error, please try again please', 
+                    temp: null, 
+                    playlist: null})
+              })      
+            }
+           else {
+            var query = 'select playlist from hot order by random() limit 1';
+            db.one(query)
+              .then(function (row) {
+                console.log(row.playlist);
+                  res.render('index', {
+                      weather: weatherText,
+                      error: null,
+                      temp: temp,
+                      playlistURL: row.playlist
+                  })
+              })
+              .catch(function (err) {
+                  res.render('index', {
+                    weather: null, 
+                    error: 'Error, please try again please', 
+                    temp: null, 
+                    playlist: null})
+              }) 
+           } 
+          }
+
+      }
+  });
+})
+
+function conversion(temp) {
+  if(temp>70){
+    return 1;
+  } else {
+    return 0;
+  }
+}
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
